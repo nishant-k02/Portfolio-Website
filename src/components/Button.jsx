5b46d09907edabd42d0f112344a998f759a9a7b3
Nishant <nishantsk2002@gmail.com>
2025-01-05 14:36:34 +0530

    Revert "add: download CV button functionality"
    This reverts commit 0bacdc9bd029b443cee4728c7f0b55e358adb730.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,91 +6,121 @@
 /**
  * Node Modules
  */
-import PropTypes from "prop-types";
+import PropTypes from "prop-types"
 
 /**
  * Primary Buttons
  */
 
-const ButtonPrimary = ({ href, target = "_self", label, icon, classes }) => {
-  if (href) {
-    return (
-      <a
-        href={href}
-        target={target}
-        className={"btn btn-primary " + classes}
-        download
-      >
-        {label}
 
-        {icon ? (
-          <span className="material-symbols-rounded" aria-hidden="true">
-            {icon}
-          </span>
-        ) : undefined}
-      </a>
-    );
-  } else {
-    return (
-      <button className={"btn btn-primary " + classes}>
-        {label}
 
-        {icon ? (
-          <span className="material-symbols-rounded" aria-hidden="true">
-            {icon}
-          </span>
-        ) : undefined}
-      </button>
-    );
-  }
-};
+const ButtonPrimary = ( {
+    href,
+    target = '_self',
+    label,
+    icon,
+    classes
+}) => {
+    if (href) {
+        return (
+            <a 
+            href={href}
+            target={target}
+            className={"btn btn-primary " + classes}
+            >
+                {label}
+
+                {icon ?
+                    <span className="material-symbols-rounded"
+                    aria-hidden="true">
+                        {icon}
+                    </span>
+                    : undefined
+                }
+            </a>
+        )
+    } else {
+        return (
+            <button className={"btn btn-primary " + classes}>
+                {label}
+
+                {icon ?
+                    <span className="material-symbols-rounded"
+                    aria-hidden="true">
+                        {icon}
+                    </span>
+                    : undefined
+                }
+            </button>
+        )
+    }
+}
 
 ButtonPrimary.propTypes = {
-  label: PropTypes.string.isRequired,
-  href: PropTypes.string,
-  target: PropTypes.string,
-  icon: PropTypes.string,
-  classes: PropTypes.string,
-};
+    label: PropTypes.string.isRequired,
+    href: PropTypes.string,
+    target: PropTypes.string,
+    icon: PropTypes.string,
+    classes: PropTypes.string
+}
 
-/**
+/** 
  * Outline Button
  */
 
-const ButtonOutline = ({ href, target = "_self", label, icon, classes }) => {
-  if (href) {
-    return (
-      <a href={href} target={target} className={"btn btn-outline " + classes}>
-        {label}
 
-        {icon ? (
-          <span className="material-symbols-rounded" aria-hidden="true">
-            {icon}
-          </span>
-        ) : undefined}
-      </a>
-    );
-  } else {
-    return (
-      <button className={"btn btn-outline " + classes}>
-        {label}
+const ButtonOutline = ( {
+    href,
+    target = '_self',
+    label,
+    icon,
+    classes
+}) => {
+    if (href) {
+        return (
+            <a 
+            href={href}
+            target={target}
+            className={"btn btn-outline " + classes}
+            >
+                {label}
 
-        {icon ? (
-          <span className="material-symbols-rounded" aria-hidden="true">
-            {icon}
-          </span>
-        ) : undefined}
-      </button>
-    );
-  }
-};
+                {icon ?
+                    <span className="material-symbols-rounded"
+                    aria-hidden="true">
+                        {icon}
+                    </span>
+                    : undefined
+                }
+            </a>
+        )
+    } else {
+        return (
+            <button className={"btn btn-outline " + classes}>
+                {label}
+
+                {icon ?
+                    <span className="material-symbols-rounded"
+                    aria-hidden="true">
+                        {icon}
+                    </span>
+                    : undefined
+                }
+            </button>
+        )
+    }
+}
 
 ButtonOutline.propTypes = {
-  label: PropTypes.string.isRequired,
-  href: PropTypes.string,
-  target: PropTypes.string,
-  icon: PropTypes.string,
-  classes: PropTypes.string,
-};
+    label: PropTypes.string.isRequired,
+    href: PropTypes.string,
+    target: PropTypes.string,
+    icon: PropTypes.string,
+    classes: PropTypes.string
+}
 
-export { ButtonPrimary, ButtonOutline };
+
+export {
+    ButtonPrimary,
+    ButtonOutline
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
         <h1>
           <a href="/" className="logo">
             <img
-              src="/src/assets/images/logo.svg"
+              src="/images/logo.svg"
               width={40}
               height={40}
               alt="Nishant Khandhar"
